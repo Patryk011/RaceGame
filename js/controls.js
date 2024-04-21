a98a36aacbe45cc34ca142eb963c9fafe69167aa
Patryk <patryknartowski@wp.pl>
2024-04-21 22:01:27 +0200

    control and surface update

--- js/controls.js
+++ js/controls.js
@@ -1,12 +1,11 @@
 import * as THREE from "three";
 import { scene, camera, renderer } from "./sceneSetup";
 import { keyboard } from "./keyboard";
-// import { createTerrain } from "./terrain";
 
-// const ground = createTerrain();
 const leftBoundary = -200;
 const rightBoundary = 200;
-const finishLineDistance = -100000;
+const finishLineDistance = -10000;
+const maxTiltAngle = THREE.MathUtils.degToRad(10);
 let gameRunning = true;
 
 const cubeGeometry = new THREE.BoxGeometry(50, 25, 60);
@@ -16,13 +15,14 @@ const cubeMaterial = new THREE.MeshBasicMaterial({
 });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cube.position.set(0, 25, -20);
+
 scene.add(cube);
 
 const clock = new THREE.Clock();
 
 export function setupControls() {
   addBoundaryLines();
-  addLines();
+
   animate();
 }
 
@@ -32,17 +32,25 @@ function updateMovement() {
 
   cube.position.z -= moveDistance;
 
-  if (keyboard.pressed("arrowleft") || keyboard.pressed("a")) {
-    cube.position.x = Math.max(cube.position.x - moveDistance, -270);
-  }
-  if (keyboard.pressed("arrowright") || keyboard.pressed("d")) {
-    cube.position.x = Math.min(cube.position.x + moveDistance, 270);
-  }
   if (keyboard.pressed("arrowup") || keyboard.pressed("w")) {
     cube.position.z -= moveDistance;
   }
   if (keyboard.pressed("arrowdown") || keyboard.pressed("s")) {
     cube.position.z += moveDistance;
+  }
+
+  if (keyboard.pressed("arrowleft") || keyboard.pressed("a")) {
+    cube.position.x = Math.max(cube.position.x - moveDistance, leftBoundary);
+    cube.rotation.z = Math.min(cube.rotation.z + delta, maxTiltAngle);
+  } else if (keyboard.pressed("arrowright") || keyboard.pressed("d")) {
+    cube.position.x = Math.min(cube.position.x + moveDistance, rightBoundary);
+    cube.rotation.z = Math.max(cube.rotation.z - delta, -maxTiltAngle);
+  } else {
+    if (cube.rotation.z < 0) {
+      cube.rotation.z = Math.min(cube.rotation.z + delta, 0);
+    } else if (cube.rotation.z > 0) {
+      cube.rotation.z = Math.max(cube.rotation.z - delta, 0);
+    }
   }
 
   camera.position.set(
@@ -52,8 +60,7 @@ function updateMovement() {
   );
   camera.lookAt(cube.position.x, cube.position.y, cube.position.z - 100);
 
-  // const speed = moveDistance / ground.geometry.parameters.width;
-  // ground.material.map.offset.y += speed * delta;
+  window.addEventListener("resize", onWindowResize, false);
 
   if (cube.position.z <= finishLineDistance) {
     stopGame();
@@ -72,46 +79,35 @@ function stopGame() {
   gameRunning = false;
 }
 
-function addBoundaryLines() {
-  const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-  const points = [];
-
-  points.push(new THREE.Vector3(leftBoundary, 0, 0));
-  points.push(new THREE.Vector3(leftBoundary, 0, finishLineDistance));
-
-  const geometryLeft = new THREE.BufferGeometry().setFromPoints(points);
-
-  const lineLeft = new THREE.Line(geometryLeft, material);
-  scene.add(lineLeft);
-
-  points[0].set(rightBoundary, 0, 0);
-  points[1].set(rightBoundary, 0, finishLineDistance);
-
-  const geometryRight = new THREE.BufferGeometry().setFromPoints(points);
-
-  const lineRight = new THREE.Line(geometryRight, material);
-  scene.add(lineRight);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function addLines() {
-  const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-  const finishLineGeometry = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(leftBoundary, 0, finishLineDistance),
-    new THREE.Vector3(rightBoundary, 0, finishLineDistance),
-  ]);
-  const finishLine = new THREE.Line(finishLineGeometry, material);
-  scene.add(finishLine);
+function addBoundaryLines() {
+  const boundaryMaterial = new THREE.LineDashedMaterial({
+    color: 0x0000ff,
+    dashSize: 3,
+    gapSize: 1,
+    scale: 1,
+  });
 
-  const roadLineGeometry = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(0, 0.1, 0),
-    new THREE.Vector3(0, 0.1, finishLineDistance),
-  ]);
+  const pointsLeft = [];
+  pointsLeft.push(new THREE.Vector3(leftBoundary, 0.1, 0));
+  pointsLeft.push(new THREE.Vector3(leftBoundary, 0.1, finishLineDistance));
 
-  const leftRoadLine = new THREE.Line(roadLineGeometry, material);
-  leftRoadLine.position.x = leftBoundary / 2;
-  scene.add(leftRoadLine);
+  const geometryLeft = new THREE.BufferGeometry().setFromPoints(pointsLeft);
+  const lineLeft = new THREE.LineSegments(geometryLeft, boundaryMaterial);
+  lineLeft.computeLineDistances();
+  scene.add(lineLeft);
 
-  const rightRoadLine = new THREE.Line(roadLineGeometry, material);
-  rightRoadLine.position.x = rightBoundary / 2;
-  scene.add(rightRoadLine);
+  const pointsRight = [];
+  pointsRight.push(new THREE.Vector3(rightBoundary, 0.1, 0));
+  pointsRight.push(new THREE.Vector3(rightBoundary, 0.1, finishLineDistance));
+
+  const geometryRight = new THREE.BufferGeometry().setFromPoints(pointsRight);
+  const lineRight = new THREE.LineSegments(geometryRight, boundaryMaterial);
+  lineRight.computeLineDistances();
+  scene.add(lineRight);
 }
--- js/terrain.js
+++ js/terrain.js
@@ -1,13 +1,38 @@
 import * as THREE from "three";
 import { scene } from "./sceneSetup.js";
 
+const leftBoundary = -600;
+const rightBoundary = 600;
+
 export function createTerrain() {
-  const asphaltMaterial = new THREE.MeshBasicMaterial({ color: 0x676767 });
+  const insideMaterial = new THREE.MeshBasicMaterial({ color: 0x676767 });
 
-  const groundGeometry = new THREE.PlaneGeometry(20000, 20000);
-  groundGeometry.rotateX(-Math.PI / 2);
+  const insideGeometry = new THREE.PlaneGeometry(400, 20000);
+  insideGeometry.rotateX(-Math.PI / 2);
 
-  const ground = new THREE.Mesh(groundGeometry, asphaltMaterial);
-  ground.position.y = -0.5;
-  scene.add(ground);
+  const insideGround = new THREE.Mesh(insideGeometry, insideMaterial);
+  insideGround.position.y = -0.5;
+  scene.add(insideGround);
+
+  const outsideMaterial = new THREE.MeshBasicMaterial({ color: 0x008000 });
+
+  const outsideLeftGeometry = new THREE.PlaneGeometry(20000, 20000);
+  outsideLeftGeometry.rotateX(-Math.PI / 2);
+  const outsideLeftGround = new THREE.Mesh(
+    outsideLeftGeometry,
+    outsideMaterial
+  );
+  outsideLeftGround.position.y = -0.5;
+  outsideLeftGround.position.x = -10000 + leftBoundary / 2;
+  scene.add(outsideLeftGround);
+
+  const outsideRightGeometry = new THREE.PlaneGeometry(20000, 20000);
+  outsideRightGeometry.rotateX(-Math.PI / 2);
+  const outsideRightGround = new THREE.Mesh(
+    outsideRightGeometry,
+    outsideMaterial
+  );
+  outsideRightGround.position.y = -0.5;
+  outsideRightGround.position.x = 10000 + rightBoundary / 2;
+  scene.add(outsideRightGround);
 }
